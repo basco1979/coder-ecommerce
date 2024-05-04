d12cdcc5dab19f7efaefce2aa974fe207a214e9b
Sebastian Basconcelo <el_basco@hotmail.com>
2024-05-04 20:20:30 -0300

    cambios en diseño

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,7 +1,7 @@
 const logoutBtn = document.getElementById('logoutBtn');
 
 logoutBtn.addEventListener('click', async (e) => {
-    const result = await fetch('http://localhost:8080/api/session/logout', {
+    const result = await fetch('/api/session/logout', {
         method: 'post',
         headers: {
             'Content-Type': 'application/json'
